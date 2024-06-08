9f79fe1d6a700926141caa595e2ecbf58f7e9b83
alikpashaevi <alikpashaev06@gmail.com>
2024-06-08 18:11:11 +0400

    Header second commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
+import { FaSearch, FaBookOpen  } from 'react-icons/fa';
+import { FaCircleXmark } from 'react-icons/fa6';
 
 const Header = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearInput = () => {
+    setInputValue('');
+  };
+
   return (
     <nav>
-      <span className='navbar-logo'><span>BookHub</span></span>
+      <span className='navbar-logo'><span><FaBookOpen />BookHub</span></span>
       <ul className='navbar-components'>
         <li className="navbar-component active">For You</li>
         <li className="navbar-component">Following</li>
       </ul>
       <form className='navbar-form' action="/">
-        <input className='navbar-input' type="text" placeholder='Search for a book'/>      
+        <FaSearch className='search-icon' />
+        <input
+          className='navbar-input'
+          type="text"
+          placeholder='Search for a book'
+          value={inputValue}
+          onChange={handleChange}
+        />
+        {inputValue && (
+          <FaCircleXmark className='clear-icon' onClick={clearInput} />
+        )}
       </form>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <sidebar>
+      <h1>Sidebar</h1>
+    </sidebar>
+  )
+}
+
+export default Sidebar
