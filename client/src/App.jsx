cf5185676f0e95bef4c1f921ca56b5ecbd4e989c
alikpashaevi <alikpashaev06@gmail.com>
2024-06-12 16:35:10 +0400

    profile updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import TrendSidebar from './components/TrendSidebar'
 import MainPage from './components/MainPage'
+import Profile from './components/Profile'
 
 const App = () => {
   return (
     <div className='container'>
      <Header /> 
      <div className='main-content'>
-        <Sidebar />
+        {/* <Sidebar />
         <MainPage />
-        <TrendSidebar />
+        <TrendSidebar /> */}
+      <Profile />
      </div>
     </div>
   )
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './Profile.css'
+import profilePic from "../assets/images/profile-pic.png";
 
 const Profile = () => {
   return (
-    <div>profile</div>
+    <>
+      <div className="profile-container">
+        <div className="profile-info">
+          <img src={profilePic} alt="profile image" />
+          <div className="profile-info-text">
+            <span className="profile-username">user.tisho123</span>
+            <span className="profile-bio">hi my name is tisho i like to read hentai books</span>
+            <span className="profile-favourite-book">tishos favorite book is: <span>yamete kudasai</span></span>
+            <span className='profile-books-read'>tisho has read 130 books this year</span>
+          </div>
+        </div>
+        <div className="profile-stats">
+          <ul className="profile-navbar">
+            <li className="main-info active" >Main info</li>
+            <li className='profile-liked'>Liked posts</li>
+          </ul>
+          <div className="profile-main-stats">
+            <h3 className='profile-title'>The books tisho has read</h3>
+            <div className="user-read-books">
+              book
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
